dea542bb77b8eae3f861b4f19b51067fa66969e2
yanomaduarte <yanduarte13@gmail.com>
2025-11-26 11:03:54 -0300

    feat: implements Fone and Contact class

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -6,15 +6,16 @@ class Fone:
     def getId(self) -> str:
         return self.__id
     
-    def getNumber(self, number: str) -> str:
-        return self.number
+    def getNumber(self) -> str:
+        return self.__number
 
-    def isValid(self, number: str)-> bool:
+    @staticmethod
+    def isValid(number: str)-> bool:
         valid_char = "0123456789()"
         for char in number:
             if char not in valid_char:
                 return False
-            return True
+        return True
         
     def __str__(self):
         return f"{self.__id}:{self.__number}"
@@ -22,18 +23,46 @@ class Fone:
 class Contact:
     def __init__(self, name: str):
         self.__name = name
-        self.fones = []
-        self.favorited = False
+        self.__fones = []
+        self.__favorited = False
+
+    def getName(self) -> str:
+        return self.__name
+    
+    def setName(self, name: str):
+        self.__name = name
+
+    def getFones(self) -> list:
+        return self.__fones
+    
+    def isFavorited(self) -> bool:
+        return self.__favorited
 
     def addFone(self, id: str, number: str):
         if Fone.isValid(number):
-            self.fones.append(Fone(id, number))
+            self.__fones.append(Fone(id, number))
         else:
             print("fail: número inválido")
 
+    def rmFone(self, index: int):
+        if 0 <= index < len(self.__fones):
+            del self.__fones[index]
+        else:
+            print(f"fail: indice {index} nao existe")
+
+    def toogleFavorited(self):
+        self.__favorited = not self.__favorited
 
     def __str__(self):
-        return f"{self.__name}"
+        prefix = "@" if self.__favorited else "-"
+
+        fones_str = []
+        for fone in self.__fones:
+            fones_str.append(str(fone))
+        
+        joined_fones = ", ".join(fones_str)
+
+        return f"{prefix} {self.__name} [{joined_fones}]"
         
 
 def main():
@@ -62,6 +91,18 @@ def main():
 
         elif cmd == "show":
             print(contact)
+
+        elif cmd == "add":
+            label = args[1]
+            number = args[2]
+            contact.addFone(label, number)
+
+        elif cmd == "rm":
+            index = int(args[1])
+            contact.rmFone(index)
+
+        elif cmd == "tfav":
+            contact.toogleFavorited()
 
 
 if __name__ == "__main__":
